fb6fab7fc1ab89715599baf6384bd2f9f4d0ea8b
Rachit Khurana <70265590+rachit1601@users.noreply.github.com>
2022-05-30 00:27:22 +0530

    secured apis with Authorization code

--- apis/views.py
+++ apis/views.py
@@ -1,5 +1,6 @@
 "All url views for Bot API"
 import json
+import os
 from django.http import HttpResponse
 from accounts.models import Profile
 # Create your views here.
@@ -7,20 +8,22 @@ from accounts.models import Profile
 
 def get_profile(request, discord_id):
     "API for getting profile"
+    if request.headers.get('Authorization') != os.getenv('API_Authorization'):
+        return HttpResponse(status=403)
     user = Profile.objects.get(discord_id=discord_id)
-    dict = {}
-    dict['username'] = user.user.username
+    data_dict = {}
+    data_dict['username'] = user.user.username
     if user.is_banned:
-        dict["ban"] = True
+        data_dict["ban"] = True
         if user.banned_reason:
-            dict["banned_reason"] = user.banned_reason
+            data_dict["banned_reason"] = user.banned_reason
         else:
-            dict["banned_reason"] = "Banned"
-        return HttpResponse(json.dumps(dict))
+            data_dict["banned_reason"] = "Banned"
+        return HttpResponse(json.dumps(data_dict))
     if user.is_public_name:
-        dict['name'] = user.name
-    dict['score'] = user.score
-    dict['current_level'] = user.current_level
+        data_dict['name'] = user.name
+    data_dict['score'] = user.score
+    data_dict['current_level'] = user.current_level
     if user.is_public_organization:
-        dict['organization'] = user.organization
-    return HttpResponse(json.dumps(dict))
+        data_dict['organization'] = user.organization
+    return HttpResponse(json.dumps(data_dict))
