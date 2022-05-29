e749057f7cbc1c15e9c4a0dd26a90519e0ff87d2
Rachit Khurana <70265590+rachit1601@users.noreply.github.com>
2022-05-29 12:27:14 +0530

    added score & banned fields to user profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+"Django Models"
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -6,11 +7,14 @@ class Profile(models.Model):
     "User model extended with more fields"
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=150, blank=True)
+    score = models.IntegerField(default=0)
     is_public_name = models.BooleanField(default=False)
     current_level = models.IntegerField(default=0)
     discord_id = models.IntegerField(default=0)
     organization = models.CharField(max_length=150, blank=True)
     is_public_organization = models.BooleanField(default=False)
+    is_banned = models.BooleanField(default=False)
+    banned_reason = models.CharField(max_length=150, blank=True)
 
     def __str__(self):
         return str(self.user)
