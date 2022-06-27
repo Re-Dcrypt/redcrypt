09da410f5744bd67aa7b691fabf0864561aa3a67
Rachit Khurana <70265590+rachit1601@users.noreply.github.com>
2022-06-27 18:51:33 +0530

    Update urls.py

--- pwa1/urls.py
+++ pwa1/urls.py
@@ -4,7 +4,6 @@ from pwa.views import manifest, service_worker, offline
 
 # Serve up serviceworker.js and manifest.json at the root
 urlpatterns = [
-    #url('serviceworker.js', service_worker, name='serviceworker'),
     url('manifest.json', manifest, name='manifest'),
     url('offline/', offline, name='offline')
 ]
