28fe196aadbb6cd3cd08a047822827905081c406
Rachit Khurana <70265590+rachit1601@users.noreply.github.com>
2022-09-04 14:09:59 +0530

    url shortner if active view

--- url_shortner/views.py
+++ url_shortner/views.py
@@ -8,15 +8,18 @@ from url_shortner.models import UrlShortner
 def redirect_url(request, slug):
     if len(UrlShortner.objects.filter(short_url=slug)) == 1:
         url_details = UrlShortner.objects.get(short_url=slug)
-        if request.user.is_authenticated and request.user not in url_details.visited_by.all():
-            url_details.visited_by.add(request.user)
-        url_details.click_counts += 1
-        url_details.save()
-        if url_details.is_content:
-            return render(
-                request,
-                'shortner.html',
-                {'content': url_details.content,
-                 'urlname': slug})
-        return redirect(url_details.full_url)
+        if url_details.active:
+            if request.user.is_authenticated and request.user not in url_details.visited_by.all():
+                url_details.visited_by.add(request.user)
+            url_details.click_counts += 1
+            url_details.save()
+            if url_details.is_content:
+                return render(
+                    request,
+                    'shortner.html',
+                    {'content': url_details.content,
+                    'urlname': slug})
+            return redirect(url_details.full_url)
+        else:
+            return redirect(f'{slug}/')
     return redirect(f'{slug}/')
