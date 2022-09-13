5a9fc4492861e9b788f25c199aa9a6e5af6fb9bc
Rachit Khurana <70265590+rachit1601@users.noreply.github.com>
2022-09-13 21:57:33 +0530

    secret key in env

--- redcrypt/settings.py
+++ redcrypt/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-p6pm-#vvtm9z0we_%98c-ji9qb$t)mqq7px3-mi+jg+yi9"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
